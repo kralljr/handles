ed7e4e5e3663d849957d7b6623eaa752423c977b
Jenna Krall <jkrall@jhsph.edu>
2014-04-29 17:40:05 -0400

    Add documentation for MDLs

---
+++ R/nycmdl.R
@@ -0,0 +1,20 @@
+#' MDLs for NYC PM2.5 constituent concentrations
+#' 
+#' A dataset of minimum detection limits (MDLs) corresponding to
+#' PM2.5 constituent concentrations from the
+#' Queens College monitor in New York City from the New York
+#' State Department of Environmental Conservation from
+#' April 2001-December 2002.  The data contains the maximum MDLs 
+#' for 30 chemical constituents of PM2.5.  The variables are:
+#' 
+#' \itemize{
+#' 		\item PM25. total mass PM2.5 concentration in mug/m^3
+#' 		\item aluminum. concentration of aluminum in mug/m^3
+#'		...
+#'}
+#'
+#' @docType data
+#' @keywords datasets
+#' @format A data frame with 1 row and 31 variables
+#' @name nydat
+NULL
