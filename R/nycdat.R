672fbea3259f91374bd81bacaed1138bf9c884dc
Jenna Krall <jkrall@jhsph.edu>
2014-04-29 17:26:19 -0400

    Add documentation for NYC QC dataset

---
+++ R/nycdat.R
@@ -0,0 +1,20 @@
+#' PM2.5 chemical constituent concentrations in New York City
+#' 
+#' A dataset of PM2.5 constituent concentrations from the
+#' Queens College monitor in New York City from the New York
+#' State Department of Environmental Conservation from
+#' April 2001-December 2002.  The data contains concentrations 
+#' for 30 chemical constituents of PM2.5.  The variables are:
+#' 
+#' \itemize{
+#' 		\item Date. date of observation
+#' 		\item PM25. total mass PM2.5 concentration in mug/m^3
+#' 		\item aluminum. concentration of aluminum in mug/m^3
+#'		...
+#'}
+#'
+#' @docType data
+#' @keywords datasets
+#' @format A data frame with 174 rows and 32 variables
+#' @name nydat
+NULL
