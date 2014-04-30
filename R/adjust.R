7e9ce30bc2fe597dee9bc24357bfd992c9326ba3
Jenna Krall <jkrall@jhsph.edu>
2014-04-29 22:01:28 -0400

    Fix ... in functions

--- R/adjust.R
+++ R/adjust.R
@@ -10,7 +10,7 @@
 #' @title adjust
 #' @param data data frame of daily constituent concentrations with date as first column
 #' @param mdl vector or matrix of MDLs for each variable of data
-#' @param method one of three methods.  'substitute' substitutes all censored constant proportion of the mdl (sub * mdl).  'exclude' excludes constituents with more than experc censored data.  'likelihood' multiply imputes censored data with a likelihood-based method.
+#' @param adjust one of three methods.  'substitute' substitutes all censored constant proportion of the mdl (sub * mdl).  'exclude' excludes constituents with more than experc censored data.  'likelihood' multiply imputes censored data with a likelihood-based method.
 #' @param sub proportion of MDL to substitute censored concentrations.  Default is 0.5.
 #' @param experc If a constituent exceeds this value, using the exclude method, the constituent will be dropped from the analysis. 
 #' @param N number of draws from posterior for likelihood-based method
@@ -30,7 +30,7 @@ adjust <- function(x, ...) UseMethod("adjust")
 
 #' @export
 adjust.default <- function(data, mdl, 
-	method = c("substitute", "exclude", "likelihood"),
+	adjust = c("substitute", "exclude", "likelihood"),
 	sub = 0.5, experc = 0.25, N = 10, burnin = 2, ...) {
 		
 	if(class(data[, 1]) != "Date") {
@@ -38,6 +38,8 @@ adjust.default <- function(data, mdl,
 	}
 	dates <- data[, 1]
 	dat <- as.matrix(data[, -1])
+	cn <- colnames(dat)
+	
 	
 	#get one MDL for each constituent	
 	if(!is.null(dim(mdl)) & dim(mdl)[1] > 1) {
@@ -57,19 +59,20 @@ adjust.default <- function(data, mdl,
 	out$call <- match.call()
 	out$bdls <- sumbdls
 	
-	if(method == "substitute") {
+	if(adjust == "substitute") {
 		dat1 <- dat * (1- bdls) + sub * mdl * bdls
 		out$sub <- sub
-	}else if(method == "exclude") {
-		cn <- colnames(dat)
-		whkeep <- cn[which(sumbdls <= experc)]
+	}else if(adjust == "exclude") {
+		
+		whkeep <- which(sumbdls <= experc)
 		if(length(whkeep) == 0) {
 			stop("Exclude method drops all constituents")
 		}
 		
-		dat1 <- subset(dat, subset = cn %in% whkeep)
-		out$exclude <- subset(cn, subset = !(cn %in% whkeep))
-	}else if(method == "likelihood") {
+		dat1 <- dat[, whkeep]
+		out$exclude <- cn[-whkeep]
+		cn <- cn[whkeep]
+	}else if(adjust == "likelihood") {
 		guess <- list()
 		guess[[1]] <- dat
 		guess[[2]] <- colMeans(dat)
@@ -84,9 +87,9 @@ adjust.default <- function(data, mdl,
 	}
 	
 	dat1 <- data.frame(dates, dat1)
-	colnames(dat1) <- c("Date", colnames(dat))
+	colnames(dat1) <- c("Date", cn)
 	out$dat <- dat1
-	out$method <- method
+	out$adjust <- adjust
 	
 	class(out) <- "adjust"
 	out
@@ -96,7 +99,7 @@ adjust.default <- function(data, mdl,
 
 
 #' @export
-print.adjust <- function(x, ...) {
+print.adjust <- function(x) {
 	cat("Call:\n")
 	print(x$call)
 	cat("Head of adjusted data:\n")
@@ -104,13 +107,13 @@ print.adjust <- function(x, ...) {
 	cat("Proportion below the MDL:\n")
 	print(x$bdls)
 	
-	if(x$method == "substitute") {
+	if(x$adjust == "substitute") {
 		cat("Substitute below MDL with MDL *\n")
 		print(x$sub)
-	}else if(x$method == "exclude") {
+	}else if(x$adjust == "exclude") {
 		cat("Constituents excluded:\n")
 		print(x$exclude)
-	}else if(x$method == "likelihood") {
+	}else if(x$adjust == "likelihood") {
 		cat("Samples: N =",  x$N, ", Burnin:", x$burnin, "\n")
 	}
 }
