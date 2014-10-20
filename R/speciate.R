b939ca79e1b94b71353bdaaea2a56f4538e4e343
Jenna Krall <jenna.krall@gmail.com>
2014-10-20 14:15:12 -0400

    Add speciation data for simulation

---
+++ R/speciate.R
@@ -0,0 +1,9 @@
+#' SPECIATE cleaned dataset of profiles
+#' 
+#' A dataset of 1744 profiles rescaled from SPECIATE and named according to one of 7 categories: oil, dust, diesel, wood, vehicle, coal, metals.  Each profile (row) represents contributions from 23 chemical constituents of PM2.5.  Each row sums to 1.  The rownames can be made into a factor of labels for k nearest neighbors.
+#'
+#' @docType data
+#' @keywords datasets
+#' @format A data frame with 1744 rows and 23 variables
+#' @name speciate
+NULL
